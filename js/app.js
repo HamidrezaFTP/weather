d696425f840b341c491d2e4e34b845c6f817f4cf
HamidrezaFTP <hamidrezaft05@gmail.com>
2025-03-22 20:50:27 +0330

    refactor weather search box and initialize JavaScript variables for search functionality

---
+++ js/app.js
@@ -0,0 +1,3 @@
+const searchInput = document.getElementById("weatherSearchBar");
+const searchButton = document.getElementById("weatherSearchBtn");
+const searchDisplay = document.getElementById("weatherDisplayBox");
